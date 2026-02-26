14d4c218775f6a39f4f90825eb069cfa7c47eef6
wonwss28-png <wonwss28@gmail.com>
2026-02-27 01:40:30 +0800

    Resolve merge conflicts and keep local files

--- obj/Debug/net10.0/Lab3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24394a6ec0126c3823e71ac08c1b5e83796d667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
